748b7e9d0f706dc74327abb607cdd3b311806f56
Roshan Bhandari <bhandariroshan362@gmail.com>
2019-07-01 22:47:25 +0545

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,20 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App />, div);it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
   ReactDOM.unmountComponentAtNode(div);
 });
